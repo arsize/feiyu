0e807a663e656a1348fb9293993739924af2041f
arsize <arsizeof3416@gmail.com>
2023-10-25 09:52:29 +0800

    feat: 新人专享

--- utils/global.js
+++ utils/global.js
@@ -1,7 +1,6 @@
 const globalData = {
   //   baseUrl: "http://gaashs.wondware.com/powerlongapp/",//测试
   //   baseUrl: "https://release.wondware.com/powerlongapp/",//预发布
-  //   baseUrl: "https://power.smartchuxing.com/powerlongappV2/",//正式
   baseUrl: "https://power.feiyuhuandian.com/powerlongappV2/",//正式
   // baseUrl: "http://106.14.67.99:8802/", //开发环境
   baseUrlImg: "http://power.feiyuhuandian.com/rechargeMinipImgV2/",
@@ -16,7 +15,7 @@ const globalData = {
   // h5baseUrl: "https://power.smartchuxing.com/",
   h5baseUrl: "https://power.feiyuhuandian.com/",
   // appId: "wx216003ee7b3ded8c",
-  // appId: "wx0e898170adfedde6",司马出行
+  // appId: "wx0e898170adfedde6",
   openLog: true,
   // qqmapkey: 'Z5MBZ-IAJR6-4PGSA-MTR6T-B6Q72-WRFR6'
   qqmapkey: "7YBBZ-PX5CQ-MLE5Z-4GCOM-6TMGS-FNFZZ",
